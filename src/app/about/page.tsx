6a658e704fe6135cfc8a973f473dd75a48f3e9f8
Subodh Acharya <subodhac.work@gmail.com>
2024-10-15 18:15:27 +0545

    added exclusive about and contact page maintenance message

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,20 +2,10 @@ import React from "react";
 import Link from "next/link";
 
 const Page = () => {
-  return (
-    <section className="pt-20 max-w-[1440px] w-full mx-auto flex justify-start items-center flex-col px-[135px] mb-[140px]">
-      <p className="text-[14px] text-left mb-20 w-full">
-        <span className="text-gray-400">
-          <Link href={"/"}>Home</Link>
-        </span>{" "}
-        /{" "}
-        <span>
-          <Link href={"/about"}>About </Link>
-        </span>
-      </p>
-      <div></div>
-    </section>
-  );
+      return <div className="max-w-[1400px] mx-auto h-40 w-full flex justify-center flex-col items-center">
+        <h1 className="text-black">About page is in maintenance.</h1>
+        <Link href="/" className="underline">Go to Homepage </Link>
+      </div>
 };
 
 export default Page;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
-  return <div>Page</div>;
+  return <div className="max-w-[1400px] mx-auto h-40 w-full flex justify-center flex-col items-center">
+    <h1 className="text-black">Contact page is in maintenance.</h1>
+    <Link href="/" className="underline">Go to Homepage </Link>
+  </div>
 };
 
 export default Page;
